b9712b271f85e0a6e0c23266ca85c2696d3db7a5
chinayz <1724162072@qq.com>
2022-05-16 14:46:33 +0800

    递归调序

---
+++ 递归调序.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int my_strlen(char *str){
+	int count = 0;
+	while(*str != '\0'){
+		count++;
+		str++;
+	}
+	return count;
+}
+void reverse_string(char arr[]){
+	int tmp = arr[0];
+	int len = my_strlen(arr);
+	arr[0] = arr[len - 1];
+	arr[len - 1] = '\0';
+	if(my_strlen(arr + 1) >= 2){
+		reverse_string(arr + 1);
+	}
+	arr[len - 1] = tmp;
+}
+int main(){
+	char arr[] = "abcdef";
+	reverse_string(arr);
+	printf("%s", arr);
+	return 0;
+}
