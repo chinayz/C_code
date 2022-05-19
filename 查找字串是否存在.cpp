2f4b2b665911fdc733a1179559c8849c671da447
chinayz <1724162072@qq.com>
2022-05-19 21:48:50 +0800

    查找字串是否存在.cpp

---
+++ 查找字串是否存在.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+#include<assert.h> 
+
+char* my_strstr(char *p1,  char *p2){
+	assert(p1 != NULL);
+	assert(p2 != NULL);
+	char *s1 = p1;
+	char *s2 = p2;
+	char *cur = p1;
+	while(*p2 == '\0'){
+		return p1;
+	}
+	while(cur != '\0'){
+		s1 = cur;
+		s2 = p2;
+		while((*s1 != '\0') && (*s2 != '\0') && (*s1 == *s2)){
+			s1++;
+			s2++;
+		}
+		if(*s2 == '\0'){
+			return cur;//找到子串 
+		}
+		cur++;
+	}
+	return NULL;//找不到子串 
+}
+
+int main(){
+	char *p1 = "abcdef";
+	char *p2 = "def";
+	char *ret = my_strstr(p1, p2);
+	if(ret == NULL){
+		printf("字串找不到\n");
+	}else{
+		printf("%s\n", ret);
+	}
+	return 0;
+}
